752dbf05d17e58d8127455cba05562d5a0469661
小妞儿 <814168332@qq.com>
2016-01-19 18:18:47 +0800

    添加钥匙串文件

--- LPYFramework/LPYFramework/GesturesUnlock(手势解锁)/Circle/LPYCircleViewConst.m
+++ LPYFramework/LPYFramework/GesturesUnlock(手势解锁)/Circle/LPYCircleViewConst.m
@@ -7,16 +7,20 @@
 //
 
 #import "LPYCircleViewConst.h"
+#import "SFHFKeychainUtils.h"
+#import "KeychainItemWrapper.h"
 
 @implementation LPYCircleViewConst
 // 偏好设置：存字符串（手势密码）
 + (void)saveGesture:(NSString *)gesture key:(NSString *)key {
+    // 偏好设置
     [[NSUserDefaults standardUserDefaults] setObject:gesture forKey:key];
     [[NSUserDefaults standardUserDefaults] synchronize];
 }
 
 // 偏好设置：取字符串手势密码
 + (NSString *)getGestureWithKey:(NSString *)key {
+    // 偏好设置
     return [[NSUserDefaults standardUserDefaults] objectForKey:key];
 }
 @end
