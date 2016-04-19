1651979a786fc0b001439c26c85f42832a99b696
小妞儿 <814168332@qq.com>
2016-04-19 19:02:07 +0800

    网络请求测试

--- UnitTestDemo/UnitTestDemoTests/UnitTestDemoTests.m
+++ UnitTestDemo/UnitTestDemoTests/UnitTestDemoTests.m
@@ -8,6 +8,7 @@
 
 #import <XCTest/XCTest.h>
 #import <AFNetworking.h>
+#import <STAlertView/STAlertView.h>
 
 // waitForExpectationsWithTimeout是等待时间，超过了就不再等待往下执行
 #define WAIT do { \
@@ -18,7 +19,10 @@
 #define NOTIFY [[NSNotificationCenter defaultCenter] postNotificationName:@"LPYBaseTest" object:nil];
 
 @interface UnitTestDemoTests : XCTestCase
-
+/**
+ *  alertView
+ */
+@property (nonatomic, strong) STAlertView *stAlertView;
 @end
 
 @implementation UnitTestDemoTests
@@ -72,7 +76,18 @@
         NSLog(@"responseObject: %@", responseObject);
         XCTAssertNotNil(responseObject, @"返回出错");
         // 继续执行
-        NOTIFY
+        self.stAlertView = [[STAlertView alloc] initWithTitle:@"验证码" message:nil textFieldHint:@"请输入手机验证码" textFieldValue:nil cancelButtonTitle:@"取消" otherButtonTitle:@"确定" cancelButtonBlock:^{
+            // 点击取消返回后执行
+            [self testAlertViewCancel];
+            // 继续执行
+            NOTIFY;
+        } otherButtonBlock:^(NSString *otherBtn) {
+            // 点击确定后执行
+            [self testAlertViewConfirm];
+            // 继续执行
+            NOTIFY;
+        }];
+        [self.stAlertView show];
     } failure:^(AFHTTPRequestOperation * _Nonnull operation, NSError * _Nonnull error) {
         NSLog(@"error: %@", error);
         XCTAssertNil(error, @"请求出错");
@@ -83,5 +98,16 @@
     WAIT
 }
 
+- (void)testAlertViewCancel {
+    NSLog(@"取消");
+}
+
+- (void)testAlertViewConfirm {
+    [self alertViewConfirm:nil];
+}
+
+- (void)alertViewConfirm:(NSString *)test {
+    NSLog(@"手机验证码：%@", test);
+}
 
 @end
